8e791259c3a0a38370f4cbcfa17d6fbd77d0c677
PintTheDragon <PintTheDragon@hotmail.com>
2020-09-12 16:24:34 -0700

    Add commands to make peanut run event work

--- EasyEvents/ScriptHandler.cs
+++ EasyEvents/ScriptHandler.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using EasyEvents.Commands;
 
 namespace EasyEvents
 {
@@ -23,42 +24,19 @@ namespace EasyEvents
                 switch (cmd)
                 {
                     case "spawn":
-                        if (args.Count < 1) throw new InvalidArgumentLengthException("Expected 1 argument but got 0 for command \"spawn\" at line "+i+".");
-
-                        var finalClassId = -1;
-                        var sum = 0;
-                        var classIds = new List<int[]>();
-
-                        for (var y = 0; y < args.Count; y++)
-                        {
-                            var argEls = args[y].Split(',');
-                            
-                            if(argEls.Length < 1) throw new InvalidArgumentException("Invalid argument for command \"spawn\" on line "+i+", argument "+y+". Expected \"(0-17),(0-100)\" but got \""+args[y]+"\".");
-                            
-                            if (argEls.Length == 1)
-                            {
-                                if (y != args.Count - 1 || finalClassId != -1) throw new InvalidArgumentException("Invalid argument for command \"spawn\" on line "+i+", argument "+y+". Expected \"(0-17),(0-100)\" but got \""+args[y]+"\".");
-
-                                    if(!int.TryParse(argEls[0], out var classId)) throw new InvalidArgumentException("Invalid argument for command \"spawn\" on line "+i+", argument "+y+". Expected \"(0-17)\" but got \""+args[y]+"\".");
-                                if(classId < 0 || classId > 17) throw new InvalidArgumentException("Invalid argument for command \"spawn\" on line "+i+", argument "+y+". Expected \"(0-17),(0-100)\" but got \""+args[y]+"\".");
-
-                                finalClassId = classId;
-                            }
-                            else if (argEls.Length == 2)
-                            {
-                                if(!int.TryParse(argEls[0], out var classId) || !int.TryParse(argEls[1], out var chance)) throw new InvalidArgumentException("Invalid argument for command \"spawn\" on line "+i+", argument "+y+". Expected \"(0-17),(0-100)\" but got \""+args[y]+"\".");
-                                if(classId < 0 || classId > 17 || chance < 0 || chance > 100) throw new InvalidArgumentException("Invalid argument for command \"spawn\" on line "+i+", argument "+y+". Expected \"(0-17),(0-100)\" but got \""+args[y]+"\".");
-
-                                sum += chance;
-                                classIds.Add(new int[] {classId, chance});
-                            }
-                            else throw new InvalidArgumentException("Invalid argument for command \"spawn\" on line "+i+", argument "+y+". Expected \"(0-17),(0-100)\" but got \""+args[y]+"\".");
-                        }
-                        
-                        if(sum > 100) throw new InvalidArgumentException("Invalid arguments for command \"spawn\" on line "+i+", argument. The sum of spawn chances should never exceed 100. Got "+sum+".");
-                        if(sum < 100 && finalClassId == -1) throw new InvalidArgumentException("Invalid arguments for command \"spawn\" on line "+i+", argument. The sum of spawn chances shouldn't be less than 100 unless you have set a class to use for the remaining players. Got "+sum+".");
-                        
-                        ScriptActions.SetCustomSpawn(classIds, finalClassId, i);
+                        Spawn.Run(args, i);
+                        break;
+                    
+                    case "roundlock":
+                        RoundSummary.RoundLock = true;
+                        break;
+                    
+                    case "detonate":
+                        AlphaWarheadController.Host.StartDetonation();
+                        break;
+                    
+                    default:
+                        throw new InvalidCommandException("The command \""+cmd+"\" on line "+i+" was not found.");
                         break;
                 }
             }
