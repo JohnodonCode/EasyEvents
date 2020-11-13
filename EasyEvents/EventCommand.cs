72d8e22d7fd2dd76b79a7771374a2c7ddef866ce
PintTheDragon <64887885+PintTheDragon@users.noreply.github.com>
2020-11-12 21:00:59 -0800

    Hopefully add SCPStats integration

--- EasyEvents/EventCommand.cs
+++ EasyEvents/EventCommand.cs
@@ -73,6 +73,7 @@ namespace EasyEvents
                 ScriptHandler.RunScript(text);
                 response = "Event \"" + command + "\" started successfully";
                 ScriptActions.scriptData.eventRan = true;
+                Loader.Plugins.FirstOrDefault(pl => pl.Name == "ScpStats")?.Assembly?.GetType("SCPStats.EventHandler")?.GetProperty("PauseRound")?.SetValue(null, true);
                 return true;
             }
             catch (Exception e)
