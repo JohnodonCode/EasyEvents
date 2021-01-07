2719c7c772cc7ebf034bf7af3fb7ddf56c289600
PintTheDragon <PintTheDragon@hotmail.com>
2021-01-07 11:43:48 -0800

    Switch to DoorVariant

--- EasyEvents/Commands/Teleport.cs
+++ EasyEvents/Commands/Teleport.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using EasyEvents.Types;
+using Interactables.Interobjects.DoorUtils;
 using UnityEngine;
 
 namespace EasyEvents.Commands
@@ -15,13 +16,13 @@ namespace EasyEvents.Commands
             if (args.Count < 4)
             {
                 var roleInfo = RoleInfo.parseRole(args[0], "teleport", i, 0);
-
-                var door = UnityEngine.Object.FindObjectsOfType<Door>().FirstOrDefault(_door => _door.DoorName.Trim().ToUpper() == args[1].Trim().ToUpper());
+                
+                var door = UnityEngine.Object.FindObjectsOfType<DoorVariant>().FirstOrDefault(_door => _door.name.Trim().ToUpper() == args[1].Trim().ToUpper());
                 if(door == null) throw new InvalidArgumentException("Invalid argument for command \"teleport\" on line "+i+", argument 1. The door name specified is not valid.");
                 
                 if (!PlayerMovementSync.FindSafePosition(door.transform.position, out var pos))
                 {
-                    throw new CommandErrorException("No safe position could be found for door \""+door.DoorName+"\".");
+                    throw new CommandErrorException("No safe position could be found for door \""+door.name+"\".");
                 }
 
                 if (args.Count == 3)
