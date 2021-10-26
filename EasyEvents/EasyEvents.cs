3648268a2fb725333f2216ccc49928a1ffa24c32
John Bostick <jbost0114@gmail.com>
2021-10-26 16:27:28 -0400

    ignore this

--- EasyEvents/EasyEvents.cs
+++ EasyEvents/EasyEvents.cs
@@ -15,7 +15,7 @@ namespace EasyEvents
     {
         public override string Name => "EasyEvents";
         public override string Author => "Johnodon";
-        public override Version Version => new Version(2, 0, 1);
+        public override Version Version => new Version(2, 2, 0);
         public override Version RequiredExiledVersion { get; } = new Version(3, 0, 0);
 
         public static EasyEvents Singleton;
